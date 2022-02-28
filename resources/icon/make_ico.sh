45034f0859096d245bce01160905f0f5c1aed40d
HactarCE <6060305+HactarCE@users.noreply.github.com>
2022-02-27 20:41:13 -0500

    Add application icon

---
+++ resources/icon/make_ico.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+convert infinite_minesweeper_16x16.png \
+        infinite_minesweeper_32x32.png \
+        infinite_minesweeper_64x64.png \
+        infinite_minesweeper_256x256.png \
+        infinite_minesweeper_512x512.png \
+        infinite_minesweeper.ico
